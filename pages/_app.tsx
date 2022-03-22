520cf0d6f325d4746fb17c61ddd7fbdcf4d772aa
SagarSubedi <ssubedi1@walsh.edu>
2022-03-22 16:01:58 -0400

    fixed a eslint error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import 'reset-css';
+import "reset-css";
 import {ChakraProvider, extendTheme} from '@chakra-ui/react';
 import PlayerLayout from '../components/playerLayout';
 
@@ -33,7 +33,7 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <PlayerLayout>
+      <PlayerLayout children=''>
       <Component {...pageProps} />
       </PlayerLayout>
     </ChakraProvider>
