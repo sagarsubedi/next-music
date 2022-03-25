bea9ce7a11686ab9c2fd08e775c968513c6e895b
Sagar Subedi <sagar@subedi.com>
2022-03-25 14:41:52 -0400

    completed profile component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,17 @@
-const Home =() => {
+import GradientLayout from "../components/gradientLayout";
+
+const Home = () => {
   return (
-      <div>HOME</div>
+    <GradientLayout
+      color="green"
+      subtitle="profile"
+      title="Sagar Subedi"
+      description="hey there!!!"
+      roundImage
+      image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
+    >
+
+    </GradientLayout>
   )
 }
 
