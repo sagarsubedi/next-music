d379359dce775273edbb26ef2b34203277600ffa
Sagar Subedi <sagar@subedi.com>
2022-03-31 14:43:47 -0400

    repeat shuffle completed

--- components/player.tsx
+++ components/player.tsx
@@ -27,13 +27,17 @@ import { formatTime } from '../lib/formatters';
 const Player = ({ songs, activeSong }) => {
 
     const [playing, setPlaying] = useState(true);
-    const [index, setIndex] = useState(0);
+    const [index, setIndex] = useState(
+        songs.findIndex(s => s.id === activeSong.id)
+    );
     const [seek, setSeek] = useState(0.0);
     const [isSeeking, setIsSeeking] = useState(false);
     const [repeat, setRepeat] = useState(false);
     const [shuffle, setShuffle] = useState(false);
     const [duration, setDuration] = useState(0.0);
     const soundRef = useRef(null);
+    const repeatRef = useRef(repeat);
+    const setActiveSong = useStoreActions((state: any) => state.changeActiveSong)
 
     useEffect(() => {
         let timerId;
@@ -48,6 +52,16 @@ const Player = ({ songs, activeSong }) => {
 
         cancelAnimationFrame(timerId);
     }, [playing, isSeeking]);
+
+    useEffect(() => {
+        setActiveSong(songs[index])
+    }, [index,])
+
+    // onEnd passed to howler will have stale value of repeat
+    // use this so that repeatRef will have most updated value of repeat
+    useEffect(() => {
+        repeatRef.current = repeat;
+    }, [repeat])
 
     const setPlayState = (value: boolean) => {
         setPlaying(value);
@@ -84,9 +98,9 @@ const Player = ({ songs, activeSong }) => {
     }
 
     const onEnd = () => {
-        if (repeat) {
+        if (repeatRef.current) {
             // seek UI back to 0
-            setSeek(0.0);
+            setSeek(0);
             // actual howler seek to 0
             soundRef.current.seek(0);
         } else {
